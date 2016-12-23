04581d3698d98e5c524cc96ed8b3ad2c7c6916d9
Miguh Ruiz <miguhruiz@gmail.com>
2016-12-23 21:27:57 +0100

    [Update] Album view

---
+++ components/loading.js
@@ -0,0 +1,12 @@
+/**
+ * Created by miguhruiz on 23/12/16.
+ */
+import React from 'react'
+
+function Loading() {
+    return (
+        <h1>Cargando datos...</h1>
+    )
+}
+
+export default Loading
--- pages/album.js
+++ pages/album.js
@@ -2,12 +2,62 @@
  * Created by miguhruiz on 23/12/16.
  */
 import React from 'react'
+import Link from 'next/link'
+
+import Loading from '../components/loading'
+
+import api from '../lib/api'
 
 class Album extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            loading: true,
+            id: props.url.query.q,
+            album: []
+        }
+    }
+    async componentDidMount() {
+        const album = await api.getAlbum(this.state.id)
+        this.setState({
+            loading: false,
+            album
+        })
+    }
     render() {
-        return(
-            <h1>It's an album!</h1>
-        )
+        if(this.state.loading) {
+            return (
+                <Loading />
+            )
+        } else {
+            return (
+                <div className="Album">
+                    <figure className="Album-cover">
+                        <img src={this.state.album.images[0].url} alt="Album Cover"/>
+                    </figure>
+                    <div className="Album-details">
+                        <h1>{this.state.album.name}</h1>
+                        <span>Por: <b>{this.state.album.artists[0].name}</b></span>
+                        <span>Popularidad: <b>{this.state.album.popularity}%</b></span>
+                        <ul className="Album-tracks">
+                            {
+                                this.state.album.tracks.items.map(
+                                    track => {
+                                        return (
+                                            <Link href={`/track?q=${track.id}`}>
+                                                <li>
+                                                    {track.track_number}.<b>{track.name}</b>
+                                                </li>
+                                            </Link>
+                                        )
+                                    }
+                                )
+                            }
+                        </ul>
+                    </div>
+                </div>
+            )
+        }
     }
 }
 
