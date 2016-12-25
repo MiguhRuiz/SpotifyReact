c5c583e7355a20c0787f79f7d54badfb7af9e024
Miguh Ruiz <miguhruiz@gmail.com>
2016-12-25 21:22:50 +0100

    [Add] Redux Store

---
+++ src/store.js
@@ -0,0 +1,21 @@
+/**
+ * Created by miguhruiz on 25/12/16.
+ */
+import { createStore, applyMiddleware } from 'redux'
+import createLogger from 'redux-logger'
+import thunk from 'redux-thunk'
+import { composeWithDevTools } from 'redux-devtools-extension'
+
+import reducer from './reducer'
+
+const store = createStore(
+    reducer,
+    composeWithDevTools(
+        applyMiddleware(
+            createLogger(),
+            thunk
+        )
+    )
+)
+
+export default store
