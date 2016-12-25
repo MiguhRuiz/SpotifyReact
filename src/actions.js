ef3ffcb53ed30a582fc2d50f5e342f2efc7a9f83
Miguh Ruiz <miguhruiz@gmail.com>
2016-12-25 21:08:01 +0100

    [Add] Redux Actions

---
+++ src/actions.js
@@ -0,0 +1,61 @@
+/**
+ * Created by miguhruiz on 25/12/16.
+ */
+import api from '../lib/api'
+
+function search(result) {
+    return {
+        type: 'SET_SEARCH',
+        payload: result
+    }
+}
+
+function setAlbum(album) {
+    return {
+        type: 'SET_ALBUM',
+        payload: album
+    }
+}
+
+function setTrack(track) {
+    return {
+        type: 'SET_TRACK',
+        payload: track
+    }
+}
+
+function setSearch(query) {
+    return async (dispatch) => {
+        const result = api.searchAlbum(query)
+        dispatch(search(result))
+
+        return result
+    }
+}
+
+function loadAlbum(id) {
+    return async (dispatch) => {
+        const album = api.getAlbum(id)
+        dispatch(setAlbum(album))
+
+        return album
+    }
+}
+
+function loadTrack(id) {
+    return async (dispatch) => {
+        const track = api.getTrack(id)
+        dispatch(setTrack(track))
+
+        return track
+    }
+}
+
+export default {
+    search,
+    setAlbum,
+    setTrack,
+    setSearch,
+    loadAlbum,
+    loadTrack
+}
